f572b44baaaa2871bafb0ce6124b25473af3d02d
namitav1997 <42519200+namitav1997@users.noreply.github.com>
2020-04-03 12:09:55 -0400

    Create hw2_seq2seq.sh

---
+++ HW2/HW2_1/hw2_seq2seq.sh
@@ -0,0 +1,4 @@
+wget -O https://drive.google.com/open?id=1gynttJ0OgDbVSqf38Y9YQeuzbfYQVOAW
+./hw2_seq2seq.sh  MLDS_hw2_1_data_testing testpgm.txt
+./hw2_seq2seq.sh  MLDS_hw2_final_data fmodel.txt
+./hw2_seq2seq.sh  MLDS_hw2_1_rndm rndm.txt
